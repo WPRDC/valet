b5efb6155b455e32952f31fb0b3698948158afbe
drw <drw@users.noreply.github.com>
2018-08-22 15:09:08 -0400

    Add commas to long transaction-count integers in table output

--- util.py
+++ util.py
@@ -76,7 +76,7 @@ def format_date(d):
 
 def format_row(hour_range,total_payments,transaction_count,utilization):
     revenue = "${:,.2f}".format(total_payments)
-    row = {'hour_range': hour_range, 'total_payments': "{}".format(revenue), 'transaction_count': transaction_count, 'utilization': "{:.3f}".format(utilization)}
+    row = {'hour_range': hour_range, 'total_payments': "{}".format(revenue), 'transaction_count': "{:,}".format(transaction_count), 'utilization': "{:.3f}".format(utilization)}
     return row
 
 def format_as_table(results):
